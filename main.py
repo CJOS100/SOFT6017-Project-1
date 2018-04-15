d12d3efa3e1a7c6df716a311b82db2d22e7f5fe7
Cathal O'Sullivan <cathal.osullivan4@mycit.ie>
2018-04-15 01:53:29 +0100

    Added code to open an account. Redid code to init the database by using global variables.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from random import randint
+
 def mainMenu():
     initDatabase()
     finished = 0
@@ -36,8 +38,35 @@ def mainMenu():
             exitProgram()
 
 def openAcc():
-    print("Works")
-    mainMenu()
+    global accountNumbers
+    global accountNames
+    global accountBalances
+    initDatabase()
+    finished = 0
+    while finished !=1:
+        firstAndLastName = input("Please enter your first and last name: ")
+        generateSuccess = 0
+        while generateSuccess !=1:
+            newAccountNumber = randint(00000, 99999)
+            if newAccountNumber in accountNumbers:
+                generateSuccess = 0
+            else:
+                generateSuccess = 1
+        initialBalance = float(input("How much would you like to deposit into your account?: "))
+        if initialBalance < 0:
+            print("Error, invalid amount, please try again")
+        else:
+            accountNumbers.append(newAccountNumber)
+            accountNames.append(firstAndLastName)
+            accountBalances.append(initialBalance)
+            filename = "bank.txt"
+            output_file = open(filename, "a")
+            output_file.write(str(newAccountNumber)+ "\n")
+            output_file.write(str(initialBalance)+ "\n")
+            output_file.write(str(firstAndLastName)+ "\n")
+            output_file.close()
+        initDatabase()
+        finished = 1
 
 def closeAcc():
     print("Works")
@@ -57,7 +86,7 @@ def genReport():
 
 def exitProgram():
     print("########################################")
-    print("Exiting")
+    print("############### Exiting ################")
     print("########################################")
     exit()
 
@@ -68,6 +97,9 @@ def initDatabase():
     databaseClean = []
     for i in range(len(database)):
         databaseClean.append(database[i].strip('\n'))
+    global accountNumbers
+    global accountBalances
+    global accountNames
     accountNumbers = databaseClean[0::3]
     accountBalances = databaseClean[1::3]
     accountNames = databaseClean[2::3]
